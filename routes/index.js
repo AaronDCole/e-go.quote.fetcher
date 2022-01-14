397584059ca764589568c017720cd0fca0fa4598
Aaron David Cole <aaron@epiphron.co.nz>
2022-01-15 00:36:48 +1300

    Added configs to launch

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,15 @@ const url = 'https://www.e-go.com.au/quote-book/';
 
 const determinedShippingQuote = (sendingSuburb, receivingSuburb, length, width, height, weight) => {
 	return new Promise((resolver, rejector) => {
-		puppeteer.launch().then(browser => {
+		puppeteer.launch({
+			headless: true,
+			args: [
+				"--disable-gpu",
+				"--disable-dev-shm-usage",
+				"--disable-setuid-sandbox",
+				"--no-sandbox",
+			]
+		}).then(browser => {
 			browser.newPage().then(page => {
 				page.setUserAgent(
 					'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
